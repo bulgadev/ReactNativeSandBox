8ea60ceaa429b98b3a9df56ddc7aed48f9bf0c71
Bulga <118321011+bulgadev@users.noreply.github.com>
2025-04-27 16:31:16 -0300

    cool stuff

--- test/App.js
+++ test/App.js
@@ -5,18 +5,27 @@ import axios from 'axios';
 import * as Device from 'expo-device';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import * as Notifications from 'expo-notifications';
-
+import * as Location from 'expo-location';
 
 
 export default function App() {
 
+  
   //use effect is to run something on the start of thecode
   React.useEffect(() => {
     //asks for notficiation premission
     const requestPermissions = async () => {
-      const { status } = await Notifications.requestPermissionsAsync();
-      if (status !== 'granted') {
+      const { status: notification } = await Notifications.requestPermissionsAsync();
+      if (notification !== 'granted') {
         alert('Permission for notifications is not granted!');
+        print('User denied location permission');	
+      }
+      
+      //we are asking permission for gps, we use status: gps cuz we can use status twice
+      const { status: GPS } = await Location.requestForegroundPermissionAsync();
+      
+      if (GPS !== 'granted') {
+        console.log("User denied location permission")
       }
     };
     requestPermissions();
@@ -24,16 +33,21 @@ export default function App() {
 
   const android = Device.osVersion;
   const user = Device.deviceName;
-
+	let location = Location.getCurrentPositionAsync({});
   //var to hold the massage
   const [message, setMessage] = useState('');
   const [name, setName] = useState('');
+  const [gps, setGps] = useState('');
   const showMessage = () => {
     setMessage(android);
   }
   const showName = () => {
-	setName(user);
+	  setName(user);
   }
+  const showLocation = () => {
+    setGps(location);
+  }
+	
   React.useEffect(() => {
     showName();
   }, []);
@@ -95,15 +109,25 @@ export default function App() {
       <TouchableHighlight
       style={styles.buttonSHOW}
       >
-     <Button
-        style={styles.buttonSHOW}
-        title="Dad? is that you?"
-        onPress={improud}
-      />
+      <Button
+         style={styles.buttonSHOW}
+         title="Dad? is that you?"
+         onPress={improud}
+       />
+       
+     </TouchableHighlight>
+     <TouchableHighlight
+      style={styles.buttonSHOW}
+      >
+      <Button
+         style={styles.buttonSHOW}
+         title="Where am I :sob:"
+         onPress={showLocation}
+       />
+     </TouchableHighlight>
  
-
-      </TouchableHighlight>
-          {message !== '' && <Text>{message}</Text>}	
+     {message !== '' && <Text>{message}</Text>}	
+     {gps !== '' && <Text>{gps}</Text>}
 	  
       <StatusBar style="auto" />
     </View>
@@ -119,8 +143,8 @@ const styles = StyleSheet.create({
   },
 
   buttonSHOW: {
-    padding: '4',
-    margin: '6',
-    gap: '15',
+    padding: 4,
+    margin: 6,
+    gap: 15,
   },
 });
